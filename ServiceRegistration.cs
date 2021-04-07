d0f2e29f361742fd3f64731b395df8b087764eab
Patekar Drazen <d.patekar@printecgroup.com>
2021-04-07 10:26:33 +0200

    Fix registration namespace

--- ServiceRegistration.cs
+++ ServiceRegistration.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using CacheAnt;
 using System.Reflection;
 
-namespace CacheAnt
+namespace Microsoft.Extensions.DependencyInjection
 {
   public static class ServiceRegistration
   {
